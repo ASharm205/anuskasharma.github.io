007157504ed58aa5ab0e35378cdba7b2767ee682
ASharm205 <148150075+ASharm205@users.noreply.github.com>
2025-09-09 12:53:02 -0400

    class example 9/9

---
+++ examples/04_js/intro.js
@@ -0,0 +1,10 @@
+/*
+const sayHello = () =>{
+    console.log('Hello, World!');
+}
+    document.getElementById("btn-click").onclick=sayHello;
+
+*/
+document.getElementById("btn-click").onclick = ()=> {
+    console.log('Hello, World!');
+}
